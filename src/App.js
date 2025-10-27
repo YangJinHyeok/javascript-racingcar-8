08e44dd75b8eaa917f1704dd743010c702492ee0
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2025-10-27 18:29:13 +0900

    feat(input): 자동차 이름 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 class App {
   async run() {}
+  async run() {
+    try {
+      const namesInput = await Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+      );
+      const names = namesInput.split(",").map((name) => name.trim());
+
 }
 
 export default App;
