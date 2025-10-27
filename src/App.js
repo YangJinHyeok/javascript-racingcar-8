1c38139243f6e3b9943a7138b71ce93bc2c1b1aa
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2025-10-27 18:30:30 +0900

    feat(object): 자동차 객체 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
+class Car {
+  constructor(name) {
+    if (name.length > 5 || name.length === 0) {
+      throw new Error("[ERROR] 자동차 이름은 1자 이상 5자 이하만 가능합니다.");
+    }
+    this.name = name;
+    this.position = 0;
+  }
+
 class App {
   async run() {}
   async run() {
@@ -6,6 +17,8 @@ class App {
         "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
       );
       const names = namesInput.split(",").map((name) => name.trim());
+
+      const cars = names.map((name) => new Car(name));
 
       const tryCountInput = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
       const tryCount = Number(tryCountInput);
